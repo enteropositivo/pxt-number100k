2c2d5ba6cc53d7387c091626cf99b6abbefb6b4a
Distintiva Solutions <57623220+distintiva@users.noreply.github.com>
2020-02-03 09:17:15 +0100

    Updates.

--- number100k.ts
+++ number100k.ts
@@ -1,9 +1,25 @@
-//% color="#4495FB" block="Basic Blocks"
+//% color="#4495FB" block="Distintiva Solutions"
 namespace distintiva {
+    let actual = 0
+    let pos = 0
+    let n = 0
+    
     //% block
-    //% x.min=0 x.max=99999
-    export function number100k(x: number) {
-
+    //% num.min=0 num.max=99999
+    export function number100k(num: number) {
+        basic.clearScreen()
+        actual = num
+        pos = 1
+        while (actual > 0) {
+            n = actual % 10
+            actual = Math.floor(actual / 10)
+            if (n > 5) {
+                led.plot(5 - pos, 0)
+                n = n - 5
+            }
+            led.plot(5 - pos, 5 - n)
+            pos += 1
+        }
     }
 }
 
